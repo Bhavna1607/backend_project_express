88c1ade7b454bf48958f860001e8a6f730502b73
Bhavna1607 <er.bhavna0710@gmail.com>
2025-08-13 17:33:46 +0530

    connect to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -6,24 +6,25 @@ dotenv.config({
 })
 
 connectDB()
-.then(()=>{
-    app.listen (process.env.PORT||8000,()=>{
-        console.log(`Server is running at port :${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("MONGO db connection failed !!!",err)
-})
+// .then(()=>{
+//     app.listen (process.env.PORT||8000,()=>{
+//         console.log(`Server is running at port :${process.env.PORT}`)
+//     })
+// })
+// .catch((err)=>{
+//     console.log("MONGO db connection failed !!!",err)
+// })
 
  
 
 /*
 import express from 'express'
 const app=express()
+
 ;(async () => { 
     try {
        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    app.on("error",()=>{
+    app.on("error",(error)=>{
         console.log("Error:",error)
         throw error
     })
